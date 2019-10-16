f5b928aa1c39ebd45bf1b797e57670ba114036d5
mirkomu <mirko.muller@he-arc.ch>
2019-10-16 08:12:09 +0200

    delete api key

--- sendMail.py
+++ sendMail.py
@@ -4,12 +4,12 @@ This call sends a message to the given recipient with vars and custom vars.
 from mailjet_rest import Client
 import os
 
-api_key = os.environ['1914512605d0595695b880ff73eab180'] api_secret = os.environ['0280288a52c20d6c759bd04be7749c4f'] mailjet = Client(auth=(api_key, api_secret), version='v3.1') data = {
+api_key = os.environ['1914512605d0595695b880ff73eab180'] api_secret = os.environ['XXX'] mailjet = Client(auth=(api_key, api_secret), version='v3.1') data = {
    'Messages': [
          {
              "From": {
                  "Email": "mirko.muller@he-arc.ch",
-                 "Name": "brun.helene@uweb.ch"
+                 "Name": "mirko "
              },
              "To": [
                  {
