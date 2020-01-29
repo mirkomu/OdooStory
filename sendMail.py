b09ceaf9613f67dae0900e239aab9e66e95f0951
mirkomu <mirko.muller@he-arc.ch>
2020-01-29 18:48:29 +0100

    disable API key

--- sendMail.py
+++ sendMail.py
@@ -4,7 +4,7 @@ This call sends a message to the given recipient with vars and custom vars.
 from mailjet_rest import Client
 import os
 
-api_key = os.environ['1914512605d0595695b880ff73eab180'] api_secret = os.environ['XXX'] mailjet = Client(auth=(api_key, api_secret), version='v3.1') data = {
+api_key = os.environ['xxx'] api_secret = os.environ['XXX'] mailjet = Client(auth=(api_key, api_secret), version='v3.1') data = {
    'Messages': [
          {
              "From": {
